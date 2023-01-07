831ef33449933f2e9d24088417c52d1c885745f2
Malith Lekamge <malithlekamge@Maliths-MacBook-Pro.local>
2023-01-08 01:30:34 +0530

    modified a print statement

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println("DSgdsfg");
 
 
 //        WestminsterSkinConsultationManager obj1 = new WestminsterSkinConsultationManager();
